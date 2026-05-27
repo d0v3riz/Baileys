ab39a6c2d0c4a36faa3c20d8733f685dcd537eb6
DwiRizqiH <a.dwirizqi.h123@gmail.com>
2026-05-27 11:34:10 +0700

    return string mexQuery

--- src/Socket/mex.ts
+++ src/Socket/mex.ts
@@ -36,7 +36,7 @@ export const executeWMexQuery = async <T>(
 	const result = await wMexQuery(variables, queryId, query, generateMessageTag)
 	const child = getBinaryNodeChild(result, 'result')
 	if (child?.content) {
-		if(child.attrs?.format === 'argo') return child.content as unknown as T
+		if(child.attrs?.format === 'argo') return child.content.toString() as unknown as T
 		const data = JSON.parse(child.content.toString())
 
 		if (data.errors && data.errors.length > 0) {
